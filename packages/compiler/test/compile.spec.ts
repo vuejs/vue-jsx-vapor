4a05c9e3b559248d42f0fa872171ae8c7280cf90
zhiyuanzmj <260480378@qq.com>
2025-04-27 00:19:10 +0800

    chore: update deps

--- packages/compiler/test/compile.spec.ts
+++ packages/compiler/test/compile.spec.ts
@@ -1,36 +1,5 @@
 import { describe, expect, test } from 'vitest'
-import {
-  transformChildren,
-  transformElement,
-  transformTemplateRef,
-  transformText,
-  transformVBind,
-  transformVHtml,
-  transformVModel,
-  transformVOn,
-  transformVShow,
-  transformVSlot,
-  transformVSlots,
-} from '../src'
-import { makeCompile } from './transforms/_utils'
-
-const compile = makeCompile({
-  nodeTransforms: [
-    transformTemplateRef,
-    transformText,
-    transformElement,
-    transformVSlot,
-    transformChildren,
-  ],
-  directiveTransforms: {
-    bind: transformVBind,
-    on: transformVOn,
-    model: transformVModel,
-    show: transformVShow,
-    html: transformVHtml,
-    slots: transformVSlots,
-  },
-})
+import { compile } from '../src'
 
 describe('compile', () => {
   test('static template', () => {
@@ -41,34 +10,17 @@ describe('compile', () => {
         <span />
       </div>`,
     )
-    expect(code).toMatchInlineSnapshot(`
-      "
-        const n0 = t0()
-        return n0
-      "
-    `)
+    expect(code).toMatchSnapshot()
   })
 
   test('dynamic root', () => {
     const { code } = compile(`<>{ 1 }{ 2 }</>`)
-    expect(code).toMatchInlineSnapshot(`
-      "
-        const n0 = _createNodes(1, 2)
-        return n0
-      "
-    `)
+    expect(code).toMatchSnapshot()
   })
 
   test('dynamic root', () => {
     const { code } = compile(`<div>{a +b +       c }</div>`)
-    expect(code).toMatchInlineSnapshot(`
-      "
-        const n0 = t0()
-        const x0 = _child(n0)
-        _setNodes(x0, () => (a +b +       c))
-        return n0
-      "
-    `)
+    expect(code).toMatchSnapshot()
   })
 
   describe('expression parsing', () => {
@@ -76,12 +28,7 @@ describe('compile', () => {
       const { code } = compile(`<>{ a + b }</>`, {
         inline: true,
       })
-      expect(code).toMatchInlineSnapshot(`
-        "
-          const n0 = _createNodes(() => (a + b))
-          return n0
-        "
-      `)
+      expect(code).toMatchSnapshot()
       expect(code).contains('a + b')
     })
   })
