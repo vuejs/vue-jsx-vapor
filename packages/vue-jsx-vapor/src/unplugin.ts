02e7575e9aa6586595d994e9a5bfdc9e9514aff1
zhiyuanzmj <260480378@qq.com>
2025-05-28 09:04:10 +0800

    chore: remove transformWithEsbuild

--- packages/vue-jsx-vapor/src/unplugin.ts
+++ packages/vue-jsx-vapor/src/unplugin.ts
@@ -1,41 +1,13 @@
 import { createUnplugin, type UnpluginFactory } from 'unplugin'
-import { createFilter, transformWithEsbuild } from 'vite'
 import plugin from './raw'
 import type { Options } from './options'
 
-export * from './options'
+export type { Options }
 
 export const unpluginFactory: UnpluginFactory<Options | undefined, true> = (
   options = {},
 ) => {
-  return [
-    ...plugin(options),
-    options.interop
-      ? { name: 'interop' }
-      : {
-          name: 'unplugin-esbuild',
-          transformInclude: createFilter(
-            options?.include || /\.[jt]sx$/,
-            options?.exclude,
-          ),
-          transform(code, id) {
-            return transformWithEsbuild(code, id, {
-              target: 'esnext',
-              charset: 'utf8',
-              minify: false,
-              minifyIdentifiers: false,
-              minifySyntax: false,
-              minifyWhitespace: false,
-              treeShaking: false,
-              keepNames: false,
-              supported: {
-                'dynamic-import': true,
-                'import-meta': true,
-              },
-            })
-          },
-        },
-  ]
+  return plugin(options)
 }
 
 export const unplugin = /* #__PURE__ */ createUnplugin(unpluginFactory)
