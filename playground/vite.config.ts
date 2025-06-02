b2f6b19d899278f2e8dc9b5d5a5608b013f64c2c
zhiyuanzmj <260480378@qq.com>
2025-06-02 20:35:54 +0800

    chore: remove vue-jsx

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import Inspect from 'vite-plugin-inspect'
 import VueJsxVapor from 'vue-jsx-vapor/vite'
-// import VueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
   plugins: [
     VueJsxVapor({
       macros: true,
     }),
-    // VueJsx(),
     Inspect(),
   ],
 })
