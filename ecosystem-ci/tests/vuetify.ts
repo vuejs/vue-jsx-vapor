b44ac6f1c2224cdfbe3e28d299b4f224772f3be7
zhiyuanzmj <260480378@qq.com>
2026-05-31 16:40:51 +0800

    chore(ecosystem-ci): stop vuetify

--- ecosystem-ci/tests/vuetify.ts
+++ ecosystem-ci/tests/vuetify.ts
@@ -2,12 +2,12 @@ import { runInRepo } from '../utils.ts'
 import type { RunOptions } from '../types.ts'
 
 export default async (options: RunOptions) => {
-  await runInRepo({
-    ...options,
-    repo: 'zhiyuanzmj/vuetify',
-    branch: 'vue-jsx-compiler',
-    install: 'pnpm install --no-frozen-lockfile',
-    env: { CI: 'false' },
-    test: 'vue-ecosystem-ci:test',
-  })
+  // await runInRepo({
+  //   ...options,
+  //   repo: 'zhiyuanzmj/vuetify',
+  //   branch: 'vue-jsx-compiler',
+  //   install: 'pnpm install --no-frozen-lockfile',
+  //   env: { CI: 'false' },
+  //   test: 'vue-ecosystem-ci:test',
+  // })
 }
