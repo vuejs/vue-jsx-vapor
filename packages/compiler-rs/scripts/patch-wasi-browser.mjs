b664485bdab0f0f6de8b1e9848d31c5ddb965d54
zhiyuanzmj <260480378@qq.com>
2026-08-24 02:13:00 +0800

    chore: patch npm/wasm32-wasi/compiler-rs.wasi-browser.js

--- packages/compiler-rs/scripts/patch-wasi-browser.mjs
+++ packages/compiler-rs/scripts/patch-wasi-browser.mjs
@@ -3,7 +3,9 @@ import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const currentDir = dirname(fileURLToPath(import.meta.url))
-const targetFiles = [resolve(currentDir, '../compiler-rs.wasi-browser.js')]
+const targetFiles = [
+  resolve(currentDir, '../npm/wasm32-wasi/compiler-rs.wasi-browser.js'),
+]
 // Remove it to fix bugs for REPL.
 const targetLine =
   'reuseWorker: { size: __asyncWorkPoolSize + __workerPoolSize },'
